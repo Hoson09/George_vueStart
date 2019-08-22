01508675d8ba9621ab147bd7d0180ec05e16a613
George_hu <huygeorge@163.com>
2019-08-22 17:14:27 +0800

    first vue

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <h1>用户管理列表页面</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>编号</th>
+          <th>姓名</th>
+          <th>地址</th>
+          <th>电话</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>huyi</td>
+          <td>beijing</td>
+          <td>1234567</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import Demo from './components/Demo'
+export default {
+  name: "app",
+  data() {
+     return{ age: 19};
+  },
+  components:{
+    'demo':Demo
+  }
+};
+</script>
+
+<style lang="scss">
+$red: red;
+#app {
+  p {
+    color: $red;
+  }
+}
+</style>
---
+++ src/components/Demo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="demo">
+    <h3>{{name}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "demo",
+  data() {
+    return {
+      name: "huyi"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.demo {
+  h3 {
+    font-size: 20px;
+    color: green;
+  }
+}
+</style>
